937a0889a3c8de3a3a7afdd0b3bd340da1310e01
Roshan Desai <rohod04@gmail.com>
2023-07-11 08:52:50 -0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  env: {
-    REACT_APP_CMS_URL: process.env.REACT_APP_CMS_URL,
-    REACT_APP_EVENT_NAME: process.env.REACT_APP_EVENT_NAME,
+  images: {
+    loader: 'akamai',
+    path: '/',
   },
 };
 
