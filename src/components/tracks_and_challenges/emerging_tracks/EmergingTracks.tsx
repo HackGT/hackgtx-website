c81fcb143b9755a4f36e83c99a23add09709afba
Roshan Desai <rohod04@gmail.com>
2023-07-11 08:41:58 -0700

    delete unneccessary files

--- src/components/tracks_and_challenges/emerging_tracks/EmergingTracks.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Text, VStack, Image } from "@chakra-ui/react";
-import TD from "../TD";
-import styles from "../TracksChallenges.module.scss";
-
-import et from "./emerging_tracks.json";
-
-const EmergingTracks = () => {};
-
-export default EmergingTracks;
--- src/components/tracks_and_challenges/general_tracks/GeneralTracks.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Image, Text, VStack } from "@chakra-ui/react";
-import TD from "../TD";
-import styles from "../TracksChallenges.module.scss";
-
-import gt from "./general_tracks.json";
-
-const GeneralTracks = () => {};
-
-export default GeneralTracks;
