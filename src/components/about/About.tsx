cef1e0e8f4fd033f5d6e527b332c9d050158d234
Roshan Desai <rohod04@gmail.com>
2023-09-12 12:46:58 -0400

    add mentor and volunteer registration dates

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -73,7 +73,7 @@ const RegisterNow = () => {
         gridArea="1 / 1"
         textAlign="center"
         alignSelf="center">
-        Registration is now open until September 18th!
+        Registration is now open until September 18th! <br /><br /> Mentor and Volunteer applications are also open <br /> until September 30th!
       </Heading>
     </Box>
   );
